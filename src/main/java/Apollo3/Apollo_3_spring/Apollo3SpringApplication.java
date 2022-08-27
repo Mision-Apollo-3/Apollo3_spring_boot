2a62b970734fa7766970e6c40477227367012f66
supe <miguelsupe09@gmail.com>
2022-08-27 14:59:49 -0500

    Creacion del proyecto spring boot

---
+++ src/main/java/Apollo3/Apollo_3_spring/Apollo3SpringApplication.java
@@ -0,0 +1,13 @@
+package Apollo3.Apollo_3_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apollo3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apollo3SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Apollo3/Apollo_3_spring/Apollo3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Apollo3.Apollo_3_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apollo3SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
