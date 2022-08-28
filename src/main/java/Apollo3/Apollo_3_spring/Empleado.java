8cdfd189e028ce4923f57f9cbf5aa01b20e4c9d8
F-FRANCO <francomartinez.duvan@gmail.com>
2022-08-28 13:38:15 -0500

    Creacion clase empleado - Duvan

---
+++ src/main/java/Apollo3/Apollo_3_spring/Empleado.java
@@ -0,0 +1,13 @@
+package Apollo3.Apollo_3_spring;
+
+import org.springframework.context.annotation.Profile;
+
+public class Empleado {
+
+    private long DocumentoIdentidad;
+    public  String Correo;
+    private Profile Profesion;
+
+
+
+}
