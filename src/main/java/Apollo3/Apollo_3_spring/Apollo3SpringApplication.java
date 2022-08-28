f04ecae6da9ade9905fcda2d18f379948ef59880
F-FRANCO <francomartinez.duvan@gmail.com>
2022-08-28 13:22:37 -0500

    prueba Duvan

--- src/main/java/Apollo3/Apollo_3_spring/Apollo3SpringApplication.java
+++ src/main/java/Apollo3/Apollo_3_spring/Apollo3SpringApplication.java
@@ -10,6 +10,7 @@ public class Apollo3SpringApplication {
 		SpringApplication.run(Apollo3SpringApplication.class, args);
 		System.out.println("miguel supelano");
 		System.out.println("Juan Gasca - 1");
+		System.out.println("Duvan");
 	}
 
 }
