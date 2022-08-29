0b96598e325515c6aa26e31bac5c548a4004cf28
JuanGasca <jpgascac@gmail.com>
2022-08-28 23:08:54 -0500

    Clase - Movimiento dinero

---
+++ src/main/java/Apollo3/Apollo_3_spring/Entidades/Movimiento_de_dinero.java
@@ -0,0 +1,44 @@
+package Apollo3.Apollo_3_spring.Entidades;
+
+public class Movimiento_de_dinero {
+    //Instanciar atributos
+    private float monto;
+    private String concepto;
+    private String usuario;
+    private String empresa;
+
+    //Método Constructor
+
+    public Movimiento_de_dinero(float monto, String concepto, String usuario, String empresa) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.usuario = usuario;
+        this.empresa = empresa;
+    }
+    // Getter and Setter
+
+    public float getMonto() {
+        return monto;
+    }
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+    public String getConcepto() {
+        return concepto;
+    }
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getEmpresa() {
+        return empresa;
+    }
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+}
