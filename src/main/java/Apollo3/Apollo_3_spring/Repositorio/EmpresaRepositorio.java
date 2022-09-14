dd55382b227c0b81edefc153dabad0b79fc1dc8a
supe <miguelsupe09@gmail.com>
2022-09-14 16:12:54 -0500

    ramas iguales

--- src/main/java/Apollo3/Apollo_3_spring/Repositorio/EmpresaRepositorio.java
+++ src/main/java/Apollo3/Apollo_3_spring/Repositorio/EmpresaRepositorio.java
@@ -1,11 +1,7 @@
 package Apollo3.Apollo_3_spring.Repositorio;
-
-
 import Apollo3.Apollo_3_spring.Entidades.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface EmpresaRepositorio extends JpaRepository <Empresa,String> {
-
-
+public interface EmpresaRepositorio extends  JpaRepository<Empresa, String>{
 }
