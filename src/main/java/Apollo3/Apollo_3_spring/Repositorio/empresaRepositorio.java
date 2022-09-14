43e3d21af4c591bc6b0e19e16992a400196ad170
supe <miguelsupe09@gmail.com>
2022-09-14 15:28:17 -0500

    merge

--- src/main/java/Apollo3/Apollo_3_spring/Repositorio/empresaRepositorio.java
+++ src/main/java/Apollo3/Apollo_3_spring/Repositorio/empresaRepositorio.java
@@ -1,9 +1,11 @@
 package Apollo3.Apollo_3_spring.Repositorio;
 
+
 import Apollo3.Apollo_3_spring.Entidades.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
-public interface empresaRepositorio extends JpaRepository<Empresa, Long> {
+public interface EmpresaRepositorio extends JpaRepository <Empresa,String> {
+
+
 }
