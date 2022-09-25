c8aa651ca429702ae526fa6faa6cb6ba6f718b23
JuanGasca <jpgascac@gmail.com>
2022-09-25 14:36:47 -0500

    Arreglos en empleadoServicio y empresaServicio que generaba error al compilar

--- src/main/java/Apollo3/Apollo_3_spring/Servicios/empleadoServicio.java
+++ src/main/java/Apollo3/Apollo_3_spring/Servicios/empleadoServicio.java
@@ -2,7 +2,7 @@ package Apollo3.Apollo_3_spring.Servicios;
 
 import Apollo3.Apollo_3_spring.Entidades.Empleado;
 import Apollo3.Apollo_3_spring.Repositorio.empleadoRepositorio;
-import Apollo3.Apollo_3_spring.Repositorio.empresaRepositorio;
+import Apollo3.Apollo_3_spring.Repositorio.EmpresaRepositorio;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,9 +11,9 @@ import java.util.ArrayList;
 public class empleadoServicio {
 
     private empleadoRepositorio empleadoRepo;
-    private empresaRepositorio empresaRepo;
+    private EmpresaRepositorio empresaRepo;
 
-    public empleadoServicio(empleadoRepositorio empleadoRepo, empresaRepositorio empresaRepo) {
+    public empleadoServicio(empleadoRepositorio empleadoRepo, EmpresaRepositorio empresaRepo) {
         this.empleadoRepo = empleadoRepo;
         this.empresaRepo = empresaRepo;
     }
@@ -24,7 +24,7 @@ public class empleadoServicio {
 
     public Empleado agregarEmpleado(long id_empresa, Empleado empleado){
 
-        empresaRepo.findById(id_empresa).map(empresa -> {
+        empresaRepo.findById(String.valueOf(id_empresa)).map(empresa -> {
             empleado.setEmpresa(empresa);
             return empresa;
         });
